b0b9c15d50b6e0b10226b9a8d0ff80121d1fb442
long <lh.goodspeed@gmail.com>
2019-07-18 20:27:33 -0400

    before bug

--- src/main/java/com/ucarrer/restaurant/restaurantlandingpagebuilder/user/UserService.java
+++ src/main/java/com/ucarrer/restaurant/restaurantlandingpagebuilder/user/UserService.java
@@ -1,4 +1,16 @@
 package com.ucarrer.restaurant.restaurantlandingpagebuilder.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("UserService")
 public class UserService {
+
+
+    UserRepository userRepository;
+
+    public User register(User user){
+
+        return userRepository.save(user);
+    }
 }
--- src/main/java/com/ucarrer/restaurant/restaurantlandingpagebuilder/user/web/UserController.java
+++ src/main/java/com/ucarrer/restaurant/restaurantlandingpagebuilder/user/web/UserController.java
@@ -1,12 +1,29 @@
 package com.ucarrer.restaurant.restaurantlandingpagebuilder.user.web;
 
+import com.ucarrer.restaurant.restaurantlandingpagebuilder.user.User;
+import com.ucarrer.restaurant.restaurantlandingpagebuilder.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
+@RestController
+@RequestMapping("/api")
+//localhost:8080/api/hello
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("hello world");
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user){
+        User newUser = userService.register(user);
+        return ResponseEntity.ok(newUser);
+    }
 
 }
