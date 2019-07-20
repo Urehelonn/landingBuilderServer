e2903eb88d199a10a13e8573775969de505f801c
deyan <zhu00118>
2019-07-20 10:23:00 -0400

    new start

--- src/main/java/com/ucarrer/restaurant/restaurantlandingpagebuilder/user/User.java
+++ src/main/java/com/ucarrer/restaurant/restaurantlandingpagebuilder/user/User.java
@@ -1,16 +1,95 @@
 package com.ucarrer.restaurant.restaurantlandingpagebuilder.user;
 
 import com.ucarrer.restaurant.restaurantlandingpagebuilder.user.enums.UserStatus;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
-import javax.persistence.Entity;
+import javax.annotation.Generated;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.security.Timestamp;
 import java.util.Date;
 
+@Entity
+@Table(name="user",uniqueConstraints={
+        @UniqueConstraint(columnNames = {"username"}),
 
+
+
+})
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @NotBlank
+    private String username;
 
+    @NotBlank
+    private String password;
 
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false)
+    @CreatedDate
+    @CreationTimestamp
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modified_at", nullable = false, columnDefinition = "TIMESTAMP  default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP")
+    @UpdateTimestamp
+    private Date modifiedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
+
+    public Date getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Date modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
--- src/main/java/com/ucarrer/restaurant/restaurantlandingpagebuilder/user/web/UserController.java
+++ src/main/java/com/ucarrer/restaurant/restaurantlandingpagebuilder/user/web/UserController.java
@@ -1,12 +1,26 @@
 package com.ucarrer.restaurant.restaurantlandingpagebuilder.user.web;
 
+import com.ucarrer.restaurant.restaurantlandingpagebuilder.user.User;
+import com.ucarrer.restaurant.restaurantlandingpagebuilder.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
+@RestController
+@RequestMapping("/api")
 public class UserController {
+    @Autowired
+    UserService userService;
+    @GetMapping("/hello")
 
+    public ResponseEntity<String>hello(){
+        return ResponseEntity.ok("hello,guy");
+    }
+
+@PostMapping("/register")
+    public ResponseEntity<User>register (@RequestBody User user) {
+User newUser= userService.register(user);
+return ResponseEntity.ok(newUser);
+}
 
 }
