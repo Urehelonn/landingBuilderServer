ebe6ef7c328c5a4444e9e671c8864a8f50550fa6
clara <claralau@cmail.carleton.ca>
2019-07-20 10:20:51 -0400

    completed login

--- src/main/java/com/ucarrer/restaurant/restaurantlandingpagebuilder/user/web/UserController.java
+++ src/main/java/com/ucarrer/restaurant/restaurantlandingpagebuilder/user/web/UserController.java
@@ -1,12 +1,43 @@
 package com.ucarrer.restaurant.restaurantlandingpagebuilder.user.web;
 
+import com.ucarrer.restaurant.restaurantlandingpagebuilder.user.User;
+import com.ucarrer.restaurant.restaurantlandingpagebuilder.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
+@RestController
+@RequestMapping("/api")
+//localhost:8080/api/hello
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World!");
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User newUser = userService.register(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        User currentUser = userService.login(user);
+        if (currentUser == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            String token = userService.creatToken(currentUser);
+            return ResponseEntity.ok(token);
+
+        }
+
+    }
 
 }
